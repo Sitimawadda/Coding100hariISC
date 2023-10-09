94ec98ce20ac485e8e6f943c066b7d466deab545
SITI MAWADDAH <146635793+Sitimawadda@users.noreply.github.com>
2023-10-09 12:24:46 +0800

    Add files via upload

---
+++ Day1/Days1.java
@@ -0,0 +1,19 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+       String nama ="siti mawaddah";
+       String Nim="D0223324";
+       int angkatan=2023;
+       char kelas='B';
+       String hobi="memasak";
+       System.out.println("nama:"+nama+"\n Nim:"+Nim+"\n angkatan:"+angkatan+"\n kelas:"+kelas+"\n hobi:"+hobi);
+       
+   
+        
+ 
+        
+      
+    }
+    
+}
